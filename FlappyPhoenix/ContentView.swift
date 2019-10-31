c36e947b22bcdb1dea6150d0ce2b7d656ea6ef2d
Richard Taylor <moomerman@gmail.com>
2019-10-31 20:32:10 +0000

    Working example with icon

--- FlappyPhoenix/ContentView.swift
+++ FlappyPhoenix/ContentView.swift
@@ -7,15 +7,40 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            VStack{
+                WebView()
+            }
+            .navigationBarTitle(Text("Flappy Phoenix"))
+            .edgesIgnoringSafeArea(.all)
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct WebView: UIViewRepresentable {
+    func makeUIView(context: Context) -> WKWebView  {
+        let webView = WKWebView()
+        webView.scrollView.isScrollEnabled = false
+        return webView
+    }
+
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let liveView = "https://flappy-phoenix.herokuapp.com/game"
+        if let url = URL(string: liveView) {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+#endif
